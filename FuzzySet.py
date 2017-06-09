4000456bf0abb8d6bc201851032500a6c44b9248
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-06-09 14:26:48 -0500

    Agrupar clases en lib

--- FuzzySet.py
+++
@@ -1,9 +0,0 @@
-class FuzzySet(object):
-    """This class contains FuzzySet capability."""
-
-    def __init__(self, name, left_interval, right_interval):
-        """Set FuzzySet initial config."""
-        super(FuzzySet, self).__init__()
-        self.name = name
-        self.left_interval = left_interval
-        self.right_interval = right_interval
--- Variable.py
+++
@@ -1,8 +0,0 @@
-class Variable(object):
-    """This class contains Variable capability."""
-
-    def __init__(self, name, fuzzy_sets=[]):
-        """Set Initial Variable config."""
-        super(Variable, self).__init__()
-        self.name = name
-        self.fuzzy_sets = fuzzy_sets
